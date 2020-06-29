51b3544c973179c519d346aca75044a5fced8a2e
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2020-06-29 14:27:21 +0200

    Local

---
+++ src/pages/modal-intercambio/modal-intercambio.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ModalIntercambioPage } from './modal-intercambio';
+
+@NgModule({
+  declarations: [
+    ModalIntercambioPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ModalIntercambioPage),
+  ],
+})
+export class ModalIntercambioPageModule {}
